cccc49a33c5c82d6f6248a8346c16b508d73f5af
AbhishekNair <114457983+AbhishekNair050@users.noreply.github.com>
2024-05-18 00:26:59 +0530

    Delete Encoder.py

--- Encoder.py
+++
@@ -1,8 +0,0 @@
-def label_encoding(data):
-    from sklearn.preprocessing import LabelEncoder
-    le = LabelEncoder()
-    for column in data.columns:
-        if data[column].dtype == 'object':
-            data[column] = le.fit_transform(data[column])
-    return data
-
