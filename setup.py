fe309836d56eb2519ce9227f0f975336a226325e
AbhishekNair <114457983+AbhishekNair050@users.noreply.github.com>
2024-05-19 22:27:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DatasetProcessor",
+    name="DatasetPreprocessor",
     version="0.1.0",
     description="A data preprocessing library for machine learning.",
     author="Abhishek Nair, Priyanshi Furiya",
